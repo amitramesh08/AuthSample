ba0031cd72b183a7ed80c2b58a53df83dc13badf
amitramesh <amit.ramesh.sharma@gmail.com>
2020-01-18 15:36:42 +0530

    Cookies Based Authentication Initial

---
+++ Auth.AuthSample/Auth.AuthSample/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auth.AuthSample.Controllers
+{
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [Route("Secret")]
+        public IActionResult Secret()
+        {
+            return Ok("Test");
+        }
+
+        [Route("AuthoizeMe")]
+        public IActionResult Authorize()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
