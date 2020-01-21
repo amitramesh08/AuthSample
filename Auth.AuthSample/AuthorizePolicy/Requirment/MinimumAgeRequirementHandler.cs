54a68f76fbb6a008427cf4e3d5955406fdc96fee
amitramesh08 <amit.ramesh.sharma@gmail.com>
2020-01-21 10:29:04 +0000

    Implement Policy Based Authentication

---
+++ Auth.AuthSample/AuthorizePolicy/Requirment/MinimumAgeRequirementHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizePolicy.Requirment
+{
+    public class MinimumAgeRequirementHandler : AuthorizationHandler<MinimumAgeRequirment>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context,
+            MinimumAgeRequirment requirement)
+        {
+            var claim = context.User.FindFirst("MinimumAge21");
+            var minimumAgeClaim1 = int.Parse(claim?.Value);
+
+            if (minimumAgeClaim1 > requirement.MinimumAge)
+            {
+                context.Succeed(requirement);
+            }
+            return null;
+        }
+    }
+}
---
+++ Auth.AuthSample/AuthorizePolicy/Requirment/MinimumAgeRequirment.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizePolicy.Requirment
+{
+    public class MinimumAgeRequirment : IAuthorizationRequirement
+    {
+        public MinimumAgeRequirment(int minimumAge)
+        {
+            MinimumAge = minimumAge;
+        }
+
+        public int MinimumAge { get; }
+    }
+}
